7bac985c3e96fdc2476caf734851437a39a5720f
Alexander Berl <a.berl@outlook.com>
2026-05-26 21:55:31 +0200

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 export { default as EventStoreHttpApi, default, createEventStoreHttpServer } from './EventStoreHttpApi.js';
 export { default as HttpEventStream } from './HttpEventStream.js';
-export { default as AsyncReadablePartition } from './AsyncReadablePartition.js';
